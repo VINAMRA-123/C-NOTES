bbddad52934ed89ff1e8d30d7833a2b44ad8bc4c
Vinamra Gupta <100684691+VINAMRA-123@users.noreply.github.com>
2023-03-06 23:09:55 +0530

    Update linearandbinaryseach.cpp

--- linearandbinaryseach.cpp
+++ linearandbinaryseach.cpp
@@ -67,3 +67,37 @@ cout<<binarysearch(arr,n,key)<<endl;
 // 67
 // 34
 // 1
+
+"Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Return the kth positive integer that is missing from this array.
+Example 1:
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+"
+"approach-
+
+    Number of missing numbers can be calculated by comparing arr[i] and i.
+    If arr[i] = 5 and i = 3, arr = [1,2,3,5], we can say that arr[i] - i - 1 = 1 numbers are missing.
+    After finding the index where arr[i] - i > k, add k to i (i + k) to find the missing number.
+    For the abovementioned example if k = 1, arr[i] - i > k at i = 3.
+    5 - 3 > 1. Answer is i + k = 3 + 1 = 4, thus 4 is missing
+"
+class Solution {
+public:
+    
+    int findKthPositive(vector<int>& arr, int k) {
+        int left = 0, right = arr.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] - mid - 1 < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left + k;
+ 
+
+    }
+};
